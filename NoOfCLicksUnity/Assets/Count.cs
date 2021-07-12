7d8a1a897bc4d4cd0289e34219727fcfdcd62978
Koushik Roy <koushik1858roy@gmail.com>
2021-07-12 15:52:54 +0530

    Project first push

---
+++ NoOfCLicksUnity/Assets/Count.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Count : MonoBehaviour
+{
+    public int count = 0;
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            count++;
+        }
+        Debug.Log(count);
+    }
+}
