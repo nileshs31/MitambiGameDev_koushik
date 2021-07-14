ba1ec1cbcb2007f1f1aadb61dd28b5b28f007208
Koushik Roy <koushik1858roy@gmail.com>
2021-07-14 18:27:27 +0530

    firebase

---
+++ NoOfCLicksUnity/Assets/Scripts/Authentication.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+using UnityEngine.UI;
+
+
+public class Authentication : MonoBehaviour
+{
+    [Header("Firebase")]
+    public DependencyStatus dependencyStatus;
+    public FirebaseAuth auth;
+    public FirebaseUser User;
+
+    [Header("Login")]
+    public InputField emailLogin;
+    public InputField passwordLogin;
+
+    [Header("Register")]
+    public InputField username;
+    public InputField emailRegistration;
+    public InputField passwordRegistration;
+
+
+    private void Awake()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            dependencyStatus = task.Result;
+            if (dependencyStatus == DependencyStatus.Available)
+            {
+                Debug.Log("Firebase Authentication ");
+                auth = FirebaseAuth.DefaultInstance;
+            }
+            else
+            {
+                Debug.LogError("Could not resolve all Firebase dependencies: " + dependencyStatus);
+            }
+        });
+    }
+
+
+    public void loginButton()
+    {
+        StartCoroutine(login(emailLogin.text, passwordLogin.text));
+    }
+
+    public void registerButton()
+    {
+        StartCoroutine(signup(username.text, emailRegistration.text, passwordRegistration.text));
+    }
+
+    public IEnumerator login(string email, string password)
+    {
+        var LogTask = auth.SignInWithEmailAndPasswordAsync(email, password);
+        yield return new WaitUntil(predicate: () => LogTask.IsCompleted);
+
+        User = LogTask.Result;
+        Debug.LogFormat("User signed in successfully: {0} ({1})", User.DisplayName, User.Email);
+        Debug.Log("Loge in");
+    }
+
+    public IEnumerator signup(string username , string email,string password)
+    {
+        if(username == "")
+        {
+            Debug.Log("username missing");
+        }
+
+        var SignTask = auth.SignInWithEmailAndPasswordAsync(email, password);
+        yield return new WaitUntil(predicate: () => SignTask.IsCompleted);
+        Debug.Log("profile is created");
+
+        User = SignTask.Result;
+
+        if (User != null)
+        {
+            UserProfile profile = new UserProfile { DisplayName = username };
+            var createProfile = User.UpdateUserProfileAsync(profile);
+
+            yield return new WaitUntil(predicate: () => createProfile.IsCompleted);
+            Debug.Log("Details Updated");
+        }
+    }
+}
---
+++ NoOfCLicksUnity/Assets/Scripts/SaveDetails.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveDetails : MonoBehaviour
+{
+    public Text usernameInput;
+    public Text emailInput;
+    public Text passwordInput;
+
+    public Text loginEmail,passwordEmail;
+
+
+    public void SubmitDetailsButton()
+    {
+        string username = usernameInput.text;
+        string email = emailInput.text;
+        string password = passwordInput.text;
+
+        PlayerPrefs.SetString("username", username);
+        PlayerPrefs.SetString("email", email);
+        PlayerPrefs.SetString("pasword", password);
+
+        Debug.Log("Data Saved");
+    }
+
+    public void loginButton()
+    {
+        if (PlayerPrefs.GetString("username")==loginEmail.text  && PlayerPrefs.GetString("password")==passwordEmail.text) {
+            
+        }
+    }
+}
