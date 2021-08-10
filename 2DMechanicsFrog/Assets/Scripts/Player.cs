76ddca012b34af2087e7b78dd4020ee58521b1e5
Koushik Roy <koushik1858roy@gmail.com>
2021-08-11 00:17:45 +0530

    parallax

--- 2DMechanicsFrog/Assets/Scripts/Player.cs
+++ 2DMechanicsFrog/Assets/Scripts/Player.cs
@@ -2,83 +2,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
-    private Vector2 positionX;
-    private bool movement;
-
-    private Rigidbody2D rb;
-    [SerializeField]
-    private float increment = 2;
+    Vector3 jump;
+    public float force;
+    Rigidbody2D rb;
 
     private Animator animPlayer;
-    private bool isIfinitePathActive = false;
 
-    private PathSpawn ps;
     public GameObject infinitepath;
-    [Range(1, 10)]
-    public float jumpVelocity;
+    private Camera mainCamera;
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
     // Start is called before the first frame update
     void Start()
     {
         animPlayer = GetComponent<Animator>();
-        rb = GetComponent<Rigidbody2D>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
+    private void FixedUpdate()
+    {
+        
+    }
 
     private void Update()
     {
-        Vector2 mousePosition = Mouse.current.position.ReadValue();
-        Debug.Log(mousePosition);
+        Debug.Log( mainCamera.ScreenToWorldPoint(Input.mousePosition));
 
-        if (Mouse.current.leftButton.wasPressedThisFrame)
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        if (Input.GetMouseButtonDown(0))
         {
-            animPlayer.SetBool("isJumping", true);
-            Doublelemovement();
+                jump = new Vector3(1, 1, 0);
+                rb.AddForce(jump * force, ForceMode2D.Impulse);
         }
-        else
+        if (Input.GetKeyDown("down"))
         {
-            animPlayer.SetBool("isJumping", false);
+            if (isGrounded)
+            {
+                jump = new Vector3(2, 2, 0);
+                rb.AddForce(jump * force, ForceMode2D.Impulse);
+            }
         }
-
-        /*
-                if (Input.GetButtonDown("SingleBtn"))
-                {
-                    animPlayer.SetBool("isJumping", true);
-                    Doublelemovement();
-                }
-                else
-                {
-                    animPlayer.SetBool("isJumping", false);
-                }
-                //double
-                if (Input.GetButtonDown("DoubleBtn"))
-                {
-                    animPlayer.SetBool("isJumping", true);
-                    Doublelemovement();
-                }
-                else
-                {
-                    animPlayer.SetBool("isJumping", false);
-                }*/
-    }
-
-    /*public void Singlemovement()
-    {
-        positionX = new Vector2(transform.position.x + increment, transform.position.y);
-        transform.position = positionX;
-    }*/
-    public void Doublelemovement()
-    {
-        positionX = new Vector2(transform.position.x + increment * 6, transform.position.y);
-        transform.position = positionX;
-       // rb.velocity = Vector2.up * jumpVelocity;
-    }
-
-    public void jump()
-    {
-         
     }
 
     public void onClickAnimation()
