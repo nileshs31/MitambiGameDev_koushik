c2ff1e629c28155d3345896627d4bdfcd3d5b2ad
Koushik Roy <koushik1858roy@gmail.com>
2021-08-16 01:38:18 +0530

    scorePrefab

--- 2DMechanicsFrog/Assets/Scripts/ScoreManager.cs
+++ 2DMechanicsFrog/Assets/Scripts/ScoreManager.cs
@@ -7,12 +7,15 @@ public class ScoreManager : MonoBehaviour
     public static ScoreManager instance;
 
     public Text scoreText;
-    public Text coinText;
     public Text highscoreText;
+    public Text coinText;
+    public Text coinsTotalText;
 
-    public float score;
-    public int coinscore;
+    public int coins = 0;
+    public int coinsTotal;
+    public float score = 0;
     public int scorePerSecond=1;
+    public float highscore;
 
     private void Awake()
     {
@@ -21,24 +24,40 @@ public class ScoreManager : MonoBehaviour
 
     private void Start()
     {
-        
+        highscore = PlayerPrefs.GetFloat("HighScore");
+       // coins = PlayerPrefs.GetInt("CoinPoint");
     }
 
     private void Update()
     {
-        coinText.text = "Coins: " + coinscore;
+        coinText.text = "Coins: " + coins;
+        coinsTotalText.text = "CoinsTotal: " + coinsTotal;
+
         score += scorePerSecond * Time.deltaTime;
         scoreText.text = "Score: " +Mathf.Round( score);
+        highscoreText.text = "HighScore: " + Mathf.Round(highscore);
+        
+        if (score > highscore)
+        {
+            highscore = score;
+            PlayerPrefs.SetFloat("HighScore", highscore);
+        }
+        if (coins > coinsTotal)
+        {
+            coinsTotal = coins;
+             PlayerPrefs.SetInt("CoinPoint", coinsTotal);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D Coin)
     {
         if (Coin.tag == "coins")
         {
-            coinscore += 5;
+            coins++;
             Destroy(Coin.gameObject);
-            scoreText.text = "Coins: " + coinscore;
+            scoreText.text = "Coins: " + coins;
         }
+            
     }
 
 }
