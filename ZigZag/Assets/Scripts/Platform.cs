5f457685b09a28614ac8c031c9fc835c666e31c7
Koushik Roy <koushik1858roy@gmail.com>
2021-12-17 10:38:17 +0530

    Infinite Spawning

---
+++ ZigZag/Assets/Scripts/Platform.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+
+    float delay = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlatformController.Instance.SpawnPlatform();
+            StartCoroutine(Fall());
+            Debug.Log("Spawn Platform");
+        }
+    }
+
+    IEnumerator Fall()
+    {
+        yield return new WaitForSeconds(delay);
+        GetComponent<Rigidbody>().isKinematic = false;
+    }
+}
---
+++ ZigZag/Assets/Scripts/PlatformController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour
+{
+    public GameObject[] platformPrefab;
+    public GameObject currentPlatform;
+
+    private static PlatformController instance;
+
+    private Stack<GameObject> leftPlatform = new Stack<GameObject>();
+    private Stack<GameObject> topPlatform = new Stack<GameObject>();
+
+    public static PlatformController Instance 
+    { 
+       get
+        {
+            if(instance == null)
+            {
+                instance = GameObject.FindObjectOfType<PlatformController>();
+            }
+            return PlatformController.instance;
+        }
+    }
+
+    void Start()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            SpawnPlatform();
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void CreatePlatform(int amount)
+    {
+
+    }
+
+    public void SpawnPlatform()
+    {
+        int randomPlatform = Random.Range(0, 2);
+        currentPlatform = (GameObject)Instantiate(platformPrefab[randomPlatform],currentPlatform.transform.GetChild(0).transform.GetChild(randomPlatform ).position,Quaternion.identity);
+        
+    }
+}
