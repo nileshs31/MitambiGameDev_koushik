3bbb1e6a79ef19444c18aabb014c5ab991f17c68
Koushik Roy <koushik1858roy@gmail.com>
2022-02-20 15:55:08 +0530

    Piano Music 3D

---
+++ Piano3DMusic/Assets/Scripts/SpawnController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    
+    [SerializeField] private GameObject[] tilePrefab;
+    [SerializeField] Transform[] spawnPoint;
+    private float timer;
+    
+    private void Start()
+    {
+        timer = 0;
+        StartCoroutine(SpawnTiles());
+    }
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        Spawntiles();
+    }
+    IEnumerator SpawnTiles()
+    {
+        yield return new WaitForSeconds(2f);
+        Spawntiles();
+    }
+
+    void Spawntiles()
+    {
+        if(timer >= 1.5f)
+        {
+            Instantiate(tilePrefab[(int)Random.Range(0f,3f)]);
+            timer = 0;
+        }
+    }
+}
---
+++ Piano3DMusic/Assets/Scripts/TilesController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TilesController : MonoBehaviour
+{
+    [SerializeField] Material tileMaterial;
+    bool mouseover;
+
+    [SerializeField] Color startColor;
+    [SerializeField] Color mouseOverColor;
+
+    private void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.back * 5f * Time.deltaTime);
+    }
+
+    void OnMouseEnter()
+    {
+        mouseover = true;
+        GetComponent<Renderer>().material.SetColor("_Color", mouseOverColor);
+    }
+
+    void OnMouseExit()
+    {
+        mouseover = false;
+        GetComponent<Renderer>().material.SetColor("_Color", startColor);
+    }
+}
